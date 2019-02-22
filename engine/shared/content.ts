8a7ee099b6eb70549dc2d1780577ebc2896a3795
jameswilddev <jameswilddev@outlook.com>
2019-02-22 06:55:51 +0000

    Added engine code stubs.

---
+++ engine/shared/content.ts
@@ -0,0 +1,29 @@
+function engineSprite(
+  atlasLeft: number,
+  atlasTop: number,
+  width: number,
+  height: number,
+  offsetLeft: number,
+  offsetTop: number
+): void {
+
+}
+
+function engineBackground(
+  id: number
+): void {
+
+}
+
+function engineSound(
+  startSamples: number,
+  durationSamples: number
+): void {
+
+}
+
+function engineSong(
+  id: number
+): void {
+
+}
