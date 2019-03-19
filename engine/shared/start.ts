660d50780097a3c4cb5b35c046a91693bd1c717f
jameswilddev <jameswilddev@outlook.com>
2019-03-19 07:55:36 +0000

    Game state is now quicksaved on close, and recovered on open (#15).

--- engine/shared/start.ts
+++ engine/shared/start.ts
@@ -1,7 +1,13 @@
 onload = function () {
   loadedGameplayCriticalContent++
   initializeSaveLoad()
-  state = initial()
+  const quicksavePath = `${localStoragePrefix}-quicksave`
+  const quicksave = loadDirect<State>(quicksavePath)
+  drop(quicksavePath)
+  state = quicksave !== undefined ? quicksave : initial()
+  onbeforeunload = () => {
+    saveDirect(quicksavePath, state)
+  }
   checkEventLoop()
 }
 checkEventLoop()
