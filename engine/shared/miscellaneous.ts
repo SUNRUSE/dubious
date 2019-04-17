8f59abd40341b42707f4c6d70ad71aa726210a5d
jameswilddev <jameswilddev@outlook.com>
2019-04-17 07:22:10 +0100

    Added Pythagoras functions.

--- engine/shared/miscellaneous.ts
+++ engine/shared/miscellaneous.ts
@@ -1,3 +1,19 @@
 function linearInterpolate(from: number, to: number, by: number): number {
   return from + (to - from) * by
 }
+
+function magnitudeSquared(x: number, y: number): number {
+  return x * x + y * y
+}
+
+function magnitude(x: number, y: number): number {
+  return Math.sqrt(magnitudeSquared(x, y))
+}
+
+function distanceSquared(x1: number, y1: number, x2: number, y2: number): number {
+  return magnitudeSquared(x2 - x1, y2 - y1)
+}
+
+function distance(x1: number, y1: number, x2: number, y2: number): number {
+  return Math.sqrt(distanceSquared(x1, y1, x2, y2))
+}
