fde0246daedd7260a6bdd34c1259e0c7b7923b43
jameswilddev <jameswilddev@outlook.com>
2019-06-23 08:10:28 +0100

    Implemented text wrapping (fixes #48).

--- engine/shared/font.ts
+++ engine/shared/font.ts
@@ -66,6 +66,73 @@ class Font {
     return text.split(`\n`).length * (this.content.layout.capHeight + this.content.layout.lineSpacing) - this.content.layout.lineSpacing
   }
 
+  wrap(text: string, width: number): string {
+    let lineWidth = 0
+    let wordWidth = 0
+    let word = ``
+    let output = ``
+    for (const character of text) {
+      if (character == `\n`) {
+        lineWidth = 0
+        wordWidth = 0
+        output += word
+        output += `\n`
+        word = ``
+      } else {
+        let characterWidth = this.characterWidth(character)
+        if (Object.prototype.hasOwnProperty.call(this.content.sprites, character)) {
+          if (lineWidth + wordWidth + characterWidth > width) {
+            if (word || lineWidth) {
+              if (lineWidth && wordWidth + characterWidth < width) {
+                output += `\n`
+                word += character
+                wordWidth += characterWidth + this.content.layout.kerning
+              } else {
+                output += word
+                output += `\n`
+                if (character != `-`) output += `-`
+                word = character
+                let dashWidth = this.characterWidth(`-`)
+                wordWidth = characterWidth + dashWidth + this.content.layout.kerning
+              }
+            } else {
+              output += character
+              output += `\n`
+            }
+            lineWidth = 0
+          } else {
+            if ([`-`, `+`, `=`, `;`, `:`, `@`, `#`, `~`, `,`, `.`].includes(character)) {
+              output += word
+              output += character
+              lineWidth += wordWidth + characterWidth + this.content.layout.kerning
+              word = ``
+              wordWidth = 0
+            } else {
+              wordWidth += characterWidth + this.content.layout.kerning
+              word += character
+            }
+          }
+        } else {
+          if (word) {
+            lineWidth += wordWidth
+            output += word
+            word = ``
+            wordWidth = 0
+          }
+          if (lineWidth + characterWidth > width) {
+            output += `\n`
+            lineWidth = 0
+          } else {
+            output += character
+            lineWidth += characterWidth + this.content.layout.kerning
+          }
+        }
+      }
+    }
+    if (word) output += word
+    return output
+  }
+
   private lineWidth(line: string): number {
     let width = 0
     for (const character of line) {
